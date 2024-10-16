8c770ea9f65b1b0249d732153584cb545e3d91e1
Tayman <taymanott@gmail.com>
2024-10-15 20:36:33 -0500

    changed name

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="bg-green-950 text-gray-200 mt-auto">
-            <p>&copy; 2024 Tayman Ott</p>
+            <p>&copy; 2024 Cephas</p>
         </footer>
     );
 };
